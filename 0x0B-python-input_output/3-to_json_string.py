c7d6d786d740af3e3571a4cf78912ea7a628f123
Desalegnbent <desalegnbenti@gmail.com>
2024-04-16 08:48:20 -0700

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines a string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
+
+0x0B-python-input_output/4-from_json_string.py
+
